1eb3e219070dc6aae3115917a338e921462fc4fb
FerLeonhardt <fnleonhardt@gmail.com>
2022-09-20 16:53:53 -0400

    setting routes and navigate in header

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
 import ForgotPassword from './pages/ForgotPassword';
 import Home from './pages/Home';
 import Offers from './pages/Offers';
@@ -10,10 +11,11 @@ function App() {
   return (
     <>
       <Router>
+        <Header />
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/profile' element={<Profile />} />
-          <Route path='/sig-in' element={<SignIn />} />
+          <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/forgot-password' element={<ForgotPassword />} />
           <Route path='/offers' element={<Offers />} />
